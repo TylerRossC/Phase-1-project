162c6bfeeeaa7a19f39df6181ded62a8ff9afa63
tyler <tylercharawell@gmail.com>
2021-05-18 09:04:38 -0700

    Updated HTML and JS to make submissionResponse() functional

--- JS/index.js
+++ JS/index.js
@@ -3,7 +3,7 @@ let jokeButton = document.querySelector("#get-joke-btn")
 let jokeDisplay = document.querySelector("#joke-display")
 let submitBtn = document.querySelector("#submit-punchline")
 let punchlineDisplay = document.querySelector("#actual-punchline")
-
+let punchline;
 
 document.addEventListener('DOMContentLoaded', function () {
     jokeButton.addEventListener('click', randomJoke)
@@ -16,20 +16,22 @@ function randomJoke() {             //Converts API into JSON
         .then(data => appendJokeSetup(data))
 }
 
-function submissionResponse() {
+function submissionResponse() {     // This function evaluates the response typed into text box and returns an alert based on the text being equal to the API's returned punchline
     let response = document.querySelector('#response')
-        if (response.innerText.toLowerCase() === punchlineDisplay.innerText.toLowerCase()) {
-            window.alert("Nailed it!")
-        } else {
-            window.alert("Think harder next time")
-        }
+    if (response.value.toLowerCase() === punchline.toLowerCase()) {
+        window.alert("Nailed it!")
+    } else {
+        window.alert("Think harder next time")
     }
-    
-    submitBtn.addEventListener('click', function() {
-        submissionResponse()
-    })
+    clearOnSubmit()                 // This line clears the response text after it has been evaluated
+}
+
+submitBtn.addEventListener('click', function () {
+    submissionResponse()
+})
 
 function appendJokeSetup(joke) {         //Displays Joke setup in desired div container on webpage
+    punchline = joke.punchline
     jokeDisplay.innerText = joke.setup
     submitBtn.addEventListener('click', appendJokePunchline)
     function appendJokePunchline() {    //This function is able to capture data from "joke" parameter because it's within the same scope and displays the punchline of "joke"
@@ -37,11 +39,13 @@ function appendJokeSetup(joke) {         //Displays Joke setup in desired div co
     }
 }
 
-function clearOnSubmit() {              //This function clears all text boxes and drop down upon click of submit and submit feedback buttons
-document.querySelector('#response').value = ""
-document.querySelector('#fname').value = ""
-document.querySelector('#lname').value = ""
-document.querySelector('#subject').value = ""
-document.querySelector('#country').value = "USA"
+function clearOnSubmit() {              //This function clears text box on click of submit 
+    document.querySelector('#response').value = ""
 }
 
+function clearOnFeedbackSubmit() {      //This function clears all text boxes and drop down on click of submit feedback button
+    document.querySelector('#fname').value = ""
+    document.querySelector('#lname').value = ""
+    document.querySelector('#subject').value = ""
+    document.querySelector('#country').value = "USA"
+}
